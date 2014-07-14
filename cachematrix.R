f2b73155bc067af84b0860f05f5b7f47f0a436be
johndharrison <johndharrison0@gmail.com>
2014-07-14 15:00:28 +0700

    Update methods

--- cachematrix.R
+++ cachematrix.R
@@ -14,8 +14,11 @@ makeCacheMatrix <- function(x = matrix()) {
     inv <<- NULL
     mChange <<- TRUE
   }
-  get <- function() m
-  setinv <- function(inverse){inv <<- inverse}
+  get <- function(){ m}
+  setinv <- function(inverse){
+    inv <<- inverse
+    mChange <<- FALSE
+  }
   getinv <- function(){inv}
   isMChange <- function(){mChange}
   list(set = set, get = get
@@ -34,5 +37,28 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  # get the inverse using getinv method from makeCacheMatrix
+  inv <- x$getinv()
+  # Ask whether the matrix has changed or not
+  isMC <- x$isMChange()
+  
+  # If the cached inverse is not NULL and
+  # if the matrix has not changed
+  if(!is.null(inv) && !isMC) {
+    message("Getting cached inverse of matrix")
+    return(inv)
+  }
+  # Get the current matrix for which inverse
+  # has been requested using get method from makeCacheMatrix
+  m <- x$get()
+  
+  # Calculate matrix inverse allow additional arguments to be passed
+  inv <- solve(m, ...)
+  # Set the calculated inverse in data structure using setinv method
+  x$setinv(inv)
+  
+  message("Getting fresh calculated inverse of matrix")
+  inv
+  
 }
